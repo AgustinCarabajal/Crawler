01a88c1d94d0e4611b8368c5a23250fd8a22aed5
aguss360@gmail.com <aguss360@gmail.com>
2018-02-02 06:34:46 -0300

    test

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-// var page = require('webpage').create();
-// page.open('http://dragonmuonline.com', function(status) {
-// 	// page.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', function() {
-// 	page.injectJs('https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js')
-// 	page.evaluate(function() {
-// 		document.GetElementById('#account').value = 'assssssssssssssssssssssss';
-
-// 	})
-// 	page.render('example.png');
-
-// 	// })
-// 	page.onLoadFinished = function(status) {
-// 		console.log('Status: ' + status);
-// 		page.render('after.png');
-// 		// Do other things here...
-// 	};
-
-// 	phantom.exit();
-// });
-
-var x = 0
-
-const wait = function(ms) {
-	var start = new Date().getTime();
-	var end = start;
-	while (end < start + ms) {
-		end = new Date().getTime();
-	}
-}
-
-var urlv = 'https://www.dragonmuonline.com/index.php?page_id=login';
-
-
-// var url = 'http://dragonmuonline.com/';
 var url = 'https://www.dragonmuonline.com/index.php?page_id=login';
 var page = require('webpage').create();
 
@@ -40,6 +6,13 @@ page.onConsoleMessage = function(msg) {
 };
 var i = 0
 
+const wait = function(ms) {
+	var start = new Date().getTime();
+	var end = start;
+	while (end < start + ms) {
+		end = new Date().getTime();
+	}
+}
 
 page.open(url, 'post', {
 	uss_id:'catwin',
@@ -64,6 +37,7 @@ page.open(url, 'post', {
 						page2.onLoadFinished = function(status) {
 							// console.log('Status 2: ' + status);
 							page2.render('test.png')
+							
 						}
 
 						page2.evaluate(function() {
@@ -86,20 +60,11 @@ page.open(url, 'post', {
 			$('input[name="uss_password"]').val('warpten513');
 			
 			$('button:contains(ENTRAR!)').click();
-			// $('form[name="login_process"]').submit();
-			// $.post('https://www.dragonmuonline.com/index.php?page_id=login', {
-			// 	uss_id:'catwin',
-			// 	uss_password:'warpten513',
-			// 	process_login:''
-
-			// })
-      // console.log('TITLE', $('title').text());
 		});
 
 		
 		
 		page.render('before.png')
-    //phantom.exit()
 });
 
 
