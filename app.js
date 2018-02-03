998300882dea8710812a8b9ded155b43f9a27f4d
aguss360@gmail.com <aguss360@gmail.com>
2018-02-02 21:05:32 -0300

    update button

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
 var url = 'https://www.dragonmuonline.com/index.php?page_id=login';
 var page = require('webpage').create();
 
+// Mensajes en consola
 page.onConsoleMessage = function(msg) {
     console.log(msg);
 };
+// Flag para que abra la segunda pagina solo una vez
 var i = 0
 
-const wait = function(ms) {
-	var start = new Date().getTime();
-	var end = start;
-	while (end < start + ms) {
-		end = new Date().getTime();
-	}
-}
-
+// Hago un posta a la pagina de login (falla y me redirecciona a otra pagina donde hago el login manual)
 page.open(url, 'post', {
 	uss_id:'catwin',
 	uss_password:'warpten513',
 	process_login:''
 }, function () {
-		page.injectJs('jquery.min.js');
-		
+		// Agrego jquery
+		page.injectJs('jquery.min.js');	
 
+		// Esto es para despues de hacer el login manual
 		page.onLoadFinished = function(status) {
 	
 			if (i === 0) {
-			// console.log('Status: ' + status);
-
+				// saco capura lo que esta haciendo el navegador
 				page.render('after.png');
 
+				// Le digo que espere y ejecute la segunda pagina (para votar)
 				setTimeout(function() {
+					// Abro otro navegador
 					var page2 = require('webpage').create();
 
 					page2.open('https://www.dragonmuonline.com/index.php?page_id=user_cp&panel=votecredits', function() {
 						page2.injectJs('jquery.min.js');
 						page2.onLoadFinished = function(status) {
-							// console.log('Status 2: ' + status);
+							// saco capura lo que esta haciendo el navegador
 							page2.render('test.png')
 							
 						}
-
+						// Aca es donde selecciono el/los botones para clickear
 						page2.evaluate(function() {
-							$('input[value="Vote Now!"]').click()
+							$('input[value="Vote Now!"]').eq(2).click()
 						})
 						
 					})
@@ -52,9 +49,9 @@ page.open(url, 'post', {
 			} else {
 				//phantom.exit()
 			}
-			// Do other things here...
 		};
 
+		// Con jquery submiteo el formulario de login
     page.evaluate(function () {
 			$('input[name="uss_id"]').val('catwin');
 			$('input[name="uss_password"]').val('warpten513');
@@ -62,8 +59,7 @@ page.open(url, 'post', {
 			$('button:contains(ENTRAR!)').click();
 		});
 
-		
-		
+		// saco capura lo que esta haciendo el navegador
 		page.render('before.png')
 });
 
