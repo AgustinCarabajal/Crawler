5b827ada1bb12b36e8afac40d051054a036f341e
aguss360@gmail.com <aguss360@gmail.com>
2018-02-06 07:09:09 -0300

    Change Button

--- app.js
+++ app.js
@@ -49,7 +49,31 @@ page.open(url, 'post', {
 			
 			}
 			else {
-				// phantom.exit()
+
+					// saco capura lo que esta haciendo el navegador
+					// page.render('after.png');
+	
+					// Le digo que espere y ejecute la segunda pagina (para votar)
+					setTimeout(function() {
+						// Abro otro navegador
+						var page3 = require('webpage').create();
+	
+						page3.open('https://www.dragonmuonline.com/index.php?page_id=user_cp&panel=votecredits', function() {
+							page3.injectJs('jquery.min.js');
+							page3.onLoadFinished = function(status) {
+								// saco capura lo que esta haciendo el navegador
+								// page3.render('test.png')
+								
+							}
+							// Aca es donde selecciono el/los botones para clickear
+							page3.evaluate(function() {
+								$('input[value="Vote Now!"]').eq(4).click()
+							})
+							
+						})
+					
+					}, 1000)
+					i++
 			}
 		};
 
